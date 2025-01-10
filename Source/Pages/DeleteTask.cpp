acbb422cc997032cee977739fec114200988eb16
Lee McLafferty <lee.mclafferty@gmail.com>
2025-01-09 21:22:13 -0800

    updates to delete

--- Source/Pages/DeleteTask.cpp
+++ Source/Pages/DeleteTask.cpp
@@ -1,6 +1,20 @@
 #include "DeleteTask.h"
+#include <iostream>
 
 delete_task::delete_task()
 {
 	pageContent = "* Delete Task* ";
+}
+
+input_state delete_task::handle_user_input()
+{
+	std::string input;
+	std::cin >> input;
+
+	return MAINMENU;
+}
+
+void delete_task::update_page_content()
+{
+	pageContent = "* Delete Task *\n\n" + display_tasks();
 }
--- Source/Pages/DeleteTask.h
+++ Source/Pages/DeleteTask.h
@@ -5,5 +5,11 @@ class delete_task : public page
 {
 public:
 	delete_task();
+
+protected:
+	virtual input_state handle_user_input() override;
+
+private:
+	void update_page_content();
 };
 
